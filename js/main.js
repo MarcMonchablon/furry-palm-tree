75137e93b7dab375bc952d8bc0cb70a3ccefcd72
pup <pup@faust.com>
2016-09-10 21:13:46 +0200

    Add throttling for ScrollEvent handler

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 (function () {
     var scrollEventCounter = 0;
-    var debouncedCallCounter = 0;
+    var scrollHandlerCallCounter = 0;
 
     var domStuff = {};
 
     $(document).ready(function() {
-	$(document).on('scroll', onScrollHandler);
+	var throttleInterval = 500;
+	var throttledScrollEventHandler = _.throttle(onScrollHandler, 
+						     throttleInterval,
+						     {trailing: true});
+
+	$(document).on('scroll', function(scrollEvent) {
+	    incrementScrollEventCounter();
+	    throttledScrollEventHandler(scrollEvent);
+	});
 	
 	domStuff.scrollEventCounter = $('#scroll-event-counter');
-	domStuff.debouncedCallCounter = $('#debounced-call-counter');
+	domStuff.scrollHandlerCallCounter = $('#scroll-handler-call-counter');
 
 	$('#reset-button').click(resetCounter);
     });
@@ -17,21 +25,23 @@
     function onScrollHandler(scrollEvent) {
 	console.log('onScrollHandler !');
 	console.log(scrollEvent);
-	incrementScrollEventCounter();
+
+	incrementScrollHandlerCallCounter();
     }
 
     function incrementScrollEventCounter() {
 	domStuff.scrollEventCounter.html(++scrollEventCounter);
     }
 
-    function incrementDebouncedCallCounter() {
-	domStuff.debouncedCallCounter.html(++debouncedCallCounter);
+    function incrementScrollHandlerCallCounter() {
+	domStuff.scrollHandlerCallCounter.html(++scrollHandlerCallCounter);
     }
 
     function resetCounter() {
 	scrollEventCounter = 0;
-	debouncedCallCounter = 0;
+	scrollHandlerCallCounter = 0;
 
 	domStuff.scrollEventCounter.html(scrollEventCounter);
+	domStuff.scrollHandlerCallCounter.html(scrollHandlerCallCounter);
     }
 })();
