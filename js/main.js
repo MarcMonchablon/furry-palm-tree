8effe6e6b4f96abdcf2fcf3f1fa25de57f6732b8
pup <pup@faust.com>
2016-09-10 19:12:44 +0200

    Add a debug zone

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
-$(function() {
-    console.log('Hello !');
-});
+(function () {
+    var scrollEventCounter = 0;
+    var debouncedCallCounter = 0;
+
+    var domStuff = {};
+
+    $(document).ready(function() {
+	$(document).on('scroll', onScrollHandler);
+	
+	domStuff.scrollEventCounter = $('#scroll-event-counter');
+	domStuff.debouncedCallCounter = $('#debounced-call-counter');
+
+	$('#reset-button').click(resetCounter);
+    });
+
+
+    function onScrollHandler(scrollEvent) {
+	console.log('onScrollHandler !');
+	console.log(scrollEvent);
+	incrementScrollEventCounter();
+    }
+
+    function incrementScrollEventCounter() {
+	domStuff.scrollEventCounter.html(++scrollEventCounter);
+    }
+
+    function incrementDebouncedCallCounter() {
+	domStuff.debouncedCallCounter.html(++debouncedCallCounter);
+    }
+
+    function resetCounter() {
+	scrollEventCounter = 0;
+	debouncedCallCounter = 0;
+
+	domStuff.scrollEventCounter.html(scrollEventCounter);
+    }
+})();
